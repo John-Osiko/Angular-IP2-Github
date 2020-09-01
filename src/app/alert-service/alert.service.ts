d29f084de15f6474f1a231a387e8f90fcb3ecbf7
John-Osiko <johnmaxosiko@gmail.com>
2020-09-01 12:15:33 +0300

    Modified class logic

---
+++ src/app/alert-service/alert.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { User } from '../user';
+import { RepoRequest } from '../repo-request';
+import { environment } from '../../environments/environment';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export declare class AlertService {
+
+  swal(): any;
+  confirm(options: any): any;
+  prompt(options: any): any;
+  alert(options: any): any;
+  question(options: any): any;
+  success(options: any): any;
+  warning(options: any): any;
+  error(options: any): any;
+  info(options: any): any;
+
+  constructor()
+}
