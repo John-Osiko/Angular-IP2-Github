c677fc9ea81990996d4170ebb4769adbba228ab9
John-Osiko <johnmaxosiko@gmail.com>
2020-09-01 08:54:10 +0300

    Added imports to root typescript template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,37 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LandingComponent } from './landing/landing.component';
+import { SearchmygitService } from './github-httpclient-service/searchmygit.service';
+import { ProfileRequestServiceService } from './profile-request-service.service';
+import { RepoRequest } from './repo-request';
+import { User } from './user';
+import { AlertService } from './alert-service/alert.service';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  { path: 'landing', component: LandingComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    RepoRequest,
+    User,
+    RouterModule.forRoot(routes)
   ],
-  providers: [],
+  providers: [SearchmygitService, ProfileRequestServiceService, AlertService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
