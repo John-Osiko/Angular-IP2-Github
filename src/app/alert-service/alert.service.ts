482d871a2023fb0efd7a596c3e7a311c553cfa7b
John-Osiko <johnmaxosiko@gmail.com>
2020-09-02 18:42:32 +0300

    Modified alert service

--- src/app/alert-service/alert.service.ts
+++ src/app/alert-service/alert.service.ts
@@ -9,17 +9,17 @@ import { environment } from '../../environments/environment';
 @Injectable({
   providedIn: 'root'
 })
-export declare class AlertService {
+export class AlertService {
 
-  swal(): any;
-  confirm(options: any): any;
-  prompt(options: any): any;
-  alert(options: any): any;
-  question(options: any): any;
-  success(options: any): any;
-  warning(options: any): any;
-  error(options: any): any;
-  info(options: any): any;
+  // swal(): any;
+  // confirm(options: any): any;
+  // prompt(options: any): any;
+  // alert(options: any): any;
+  // question(options: any): any;
+  // success(options: any): any;
+  // warning(options: any): any;
+  // error(options: any): any;
+  // info(options: any): any;
 
-  constructor()
+  constructor(){}
 }
